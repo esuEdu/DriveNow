5f27ba2f63bdec52a122dcadaae377673669b164
Eduardo R Vieira <eduardovieirayt@gmail.com>
2024-12-07 13:01:11 -0300

    feat: endpoints

---
+++ DriveNow/DriveNow/Networking/Endpoints.swift
@@ -0,0 +1,27 @@
+//
+//  Untitled.swift
+//  DriveNow
+//
+//  Created by Eduardo on 07/12/24.
+//
+
+import Foundation
+
+enum Endpoints {
+    static let baseURL: String = "https://xd5zl5kk2yltomvw5fb37y3bm40vsyrx.lambda-url.sa-east-1.on.aws"
+    
+    case postRideEstimate
+    case patchRideConfirm
+    case getRide(customerId: String, driverId: String)
+    
+    var url: URL {
+        switch self {
+            case .postRideEstimate:
+                return URL(string: "\(Endpoints.baseURL)/ride/estimate")!
+            case .patchRideConfirm:
+                return URL(string: "\(Endpoints.baseURL)/ride/confirm")!
+            case .getRide(let customerId, let driverId):
+                return URL(string: "\(Endpoints.baseURL)/ride/\(customerId)?driver_id=\(driverId)")!
+        }
+    }
+}
